cd541f46f70f946e0d2c27b98b5446b7d24d36dc
tvergho <tvergho@gmail.com>
2021-01-15 13:01:47 -0800

    Styling updates

--- components/SidebarLayout/index.js
+++ components/SidebarLayout/index.js
@@ -10,21 +10,10 @@ const SidebarLayout = ({ children }) => {
   );
 };
 
-SidebarLayout.Main = () => {
+SidebarLayout.Main = ({ children }) => {
   return (
     <div className={styles.main}>
-      <p>The security of your possessions is our primary concern.
-        Our facilities are safeguarded behind 7 foot fencing topped with barb wire and extensive lighting, and our entire facility is under constant 24 hour closed circuit camera surveillance.
-      </p>
-
-      <h2>You Lock It, You Keep the Key</h2>
-      <p>You can be sure your space is private and secure as you are the only person with the key to your storage space.</p>
-
-      <h2>Daily Computerized Gate Entry</h2>
-      <p>Access your storage unit when you need it. To enter or exit the facility, each tenant receives a personalized security code to be entered into a lighted key pad at the main entrance gate.
-        This helps ensure that only tenants have access to the property. The system records all entries and exits.
-        Enter your unique access code at the front gate to enter the Brosterhous Storage facility from 5:30am-10pm each day.
-      </p>
+      {children}
     </div>
   );
 };
@@ -40,6 +29,10 @@ SidebarLayout.Sidebar = ({ title, quotes }) => {
 };
 
 SidebarLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+SidebarLayout.Main.propTypes = {
   children: PropTypes.node,
 };
 
--- pages/security.js
+++ pages/security.js
@@ -15,7 +15,20 @@ const SecurityPage = () => {
       />
       <PageTitle title="Security" />
       <SidebarLayout>
-        <SidebarLayout.Main />
+        <SidebarLayout.Main>
+          <p>The security of your possessions is our primary concern.
+            Our facilities are safeguarded behind 7 foot fencing topped with barb wire and extensive lighting, and our entire facility is under constant 24 hour closed circuit camera surveillance.
+          </p>
+
+          <h2>You Lock It, You Keep the Key</h2>
+          <p>You can be sure your space is private and secure as you are the only person with the key to your storage space.</p>
+
+          <h2>Daily Computerized Gate Entry</h2>
+          <p>Access your storage unit when you need it. To enter or exit the facility, each tenant receives a personalized security code to be entered into a lighted key pad at the main entrance gate.
+            This helps ensure that only tenants have access to the property. The system records all entries and exits.
+            Enter your unique access code at the front gate to enter the Brosterhous Storage facility from 5:30am-10pm each day.
+          </p>
+        </SidebarLayout.Main>
         <SidebarLayout.Sidebar title={title} quotes={quotes} />
       </SidebarLayout>
     </>
