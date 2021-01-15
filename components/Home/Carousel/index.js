47f48f9f6c1747aef1828216e5faab58fae16c12
tvergho <tvergho@gmail.com>
2021-01-14 20:05:51 -0800

    Completed home carousel

---
+++ components/Home/Carousel/index.js
@@ -0,0 +1,53 @@
+/* eslint-disable new-cap */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Carousel from 'nuka-carousel';
+import Image from 'components/Image';
+import { LeftArrow, RightArrow } from './assets';
+import styles from './styles.module.scss';
+
+const SlideButton = ({ asset: Asset, onClick }) => {
+  return (
+    <button type="button" onClick={onClick} className={styles['slide-button']}>
+      <Asset />
+    </button>
+  );
+};
+
+const ImageCarousel = ({ images, width, height }) => {
+  return (
+    <section className="container">
+      <div className={styles['carousel-container']}>
+        <Carousel
+          wrapAround
+          autoplay
+          autoplayInterval={5000}
+          width={width}
+          height={height}
+          renderCenterLeftControls={({ previousSlide }) => (<SlideButton asset={LeftArrow} onClick={previousSlide} />)}
+          renderCenterRightControls={({ nextSlide }) => (<SlideButton asset={RightArrow} onClick={nextSlide} />)}
+        >
+          {images.map(({ src, description }) => (
+            <div className={styles['image-container']}>
+              <div className={styles.description}>
+                <p>{description}</p>
+              </div>
+              <Image responsive src={src} key={description} objectFit="cover" />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </section>
+  );
+};
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+    description: PropTypes.string,
+  })),
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default ImageCarousel;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { SITE_NAME } from 'lib/constants';
+import { SITE_NAME, HOME_IMAGES } from 'lib/constants';
 import { NextSeo } from 'next-seo';
+import { Carousel } from 'components/Home';
 
 const Home = () => {
   return (
     <>
       <NextSeo
         title={`Home | ${SITE_NAME}`}
-        description="Description."
+        description="Ready for some spring cleaning? We'll make sure your extra furniture, files, or appliances are safely stored until you need them again."
       />
+      <Carousel width="100%" height="100%" images={HOME_IMAGES} />
     </>
   );
 };
